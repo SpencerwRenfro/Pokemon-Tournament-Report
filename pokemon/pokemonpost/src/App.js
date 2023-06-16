3ba01730ef6b54efbeccbd56fd27f2ca8d6aed8e
rifraf94 <spencer.renfro13@gmail.com>
2023-06-16 09:23:39 -0600

    second commit- installed tailwind

---
+++ pokemon/pokemonpost/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Report from './Report'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">App</h1>
+      <Report />
+    </div>
+  )
+}
