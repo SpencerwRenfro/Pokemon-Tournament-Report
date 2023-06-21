703072ea3ec22909d2632a015da51e9146ab208b
TylerRenfro <v.renfro6@gmail.com>
2023-06-20 20:13:59 -0600

    adds front-end design to mew comparison

--- pokemon/pokemonpost/src/components/MewComparison/MewComparison.js
+++ pokemon/pokemonpost/src/components/MewComparison/MewComparison.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function MewComparison(props) {
-    let overteching = `${process.env.PUBLIC_URL}/images/Mew/overteching.svg`
-    let penny = `${process.env.PUBLIC_URL}/images/Mew/penny.png`
-    let nest = `${process.env.PUBLIC_URL}/images/Mew/nest.png`
+function MewComparison(props) {    
+    let nest = `${process.env.PUBLIC_URL}/images/Cards/nest-ball.png`
+    let penny = `${process.env.PUBLIC_URL}/images/Cards/penny.png`    
+    let overteching = `${process.env.PUBLIC_URL}/images/Cards/tech-group.png`
     return (
         <div id={props.mewComparison}>
             <h1>Top 8 Mew Comparison</h1>
@@ -356,47 +356,41 @@ function MewComparison(props) {
             </div>
 
 
-            <p>To articulate why I think the list is bad it mainly comes down to Feather Ball over Nest Ball and Supporters. Without going into a full-blown autistic rant about four cards, below is a brief summary on my reasoning.</p>
+            <p className="leading-7 mb-10">To articulate why I think the list is bad it mainly comes down to Feather Ball over Nest Ball and Supporters. Without going into a full-blown autistic rant about four cards, below is a brief summary on my reasoning.</p>
             {/* grid layouts */}
-            <div className="grid grid-cols-12 ">
-                <div className="col-span-12 
-                                lg:col-start-1 lg:col-end-5
-                                xl:col-start-1 xl:col-end-5">
-                    <img src={nest} alt="nest ball" />
+            <div className="grid grid-cols-12 gap-8">
+                <div className="col-span-4">
+                    <img className="w-full" src={nest} alt="nest ball" />
                 </div>
-                <div className="col-span-12 
-                                lg:col-start-6 lg:col-end-12
-                                xl:col-start-6 xl:col-end-12 ">
-                    <h1 className="justify-center">Nest Ball is Overrated</h1>
-                    <p>With a full bench, Nest Ball can only be Played down with Ultra Ball or Cram-o-matic. Whereas Feather Ball is playable 100% of the time—regardless of board state.</p>
-                    <p>Additionally, Feather Balls are amazing for grabbing Mews, giving Ultra Ball extra flexibility to fetch Genesect.</p>
-                </div>
-                <div className="col-span-12 
-                                lg:col-start-1 lg:col-end-5
-                                xl:col-start-1 xl:col-end-5">
-                    <img src={penny} alt="nest ball" />
-                </div>
-                <div className="col-span-12 
-                                lg:col-start-6 lg:col-end-12
-                                xl:col-start-6 xl:col-end-12 ">
-                    <h1 className="justify-center">Penny is Mid</h1>
-                    <p>There are situations where Penny has value to save a Genesect and remove accumulating damage. However, if you play Penny, you are not disrupting with Judge and are allowing your opponent sit on large hand sizes. </p>
-                    <p>It’s stronger to try and disrupt whatever combo your opponent is attempting to build in their hand.  Also it’s one matchup.</p>
-                </div>
-                <div className="col-span-12 
-                                lg:col-start-1 lg:col-end-5
-                                xl:col-start-1 xl:col-end-5">
-                    <img src={overteching} alt="nest ball" />
-                </div>
-                <div className="col-span-12 
-                                lg:col-start-6 lg:col-end-12
-                                xl:col-start-6 xl:col-end-12 ">
-                    <h1 className="justify-center">Overteching</h1>
-                    <p>Specific to Le Bui, his list is over teched for no reason. I don’t really see a benefit for playing Avery over Judge in the format. Even in our match I was relieved to see Avery as the supporter for turn instead of the Judge + Path combo. Fan of Waves seems like a complete wasted that could have been better as a third Forest Seal Stone. Two copies is risky as you want to play it every game—especially since your opponents will attempt to Lost Zone them.</p>
-
+                <div className="col-span-8">
+                    <h3 className="text-2xl font-bold mb-5">Nest Ball is Overrated</h3>
+                    <p className="leading-7 mb-5">With a full bench, Nest Ball can only be Played down with Ultra Ball or Cram-o-matic. Whereas Feather Ball is playable 100% of the time—regardless of board state.</p>
+                    <p className="leading-7 mb-5">Additionally, Feather Balls are amazing for grabbing Mews, giving Ultra Ball extra flexibility to fetch Genesect.</p>
                 </div>
             </div>
-            <p>I fully believe that with better draws, or in the command of a better pilot Tord’s list could have made top 8 or even taken down the entire event. Day one felt so smooth and perfect, but my hands in day two were packed with bad luck, rough opening hand decisions, and every round I was forced to discard resources that I knew I couldn't to win.</p>
+            <div className="divider my-5"></div>
+            <div className="grid grid-cols-12 gap-8">
+                <div className="col-span-4">
+                    <img className="w-full" src={penny} alt="Penny" />
+                </div>
+                <div className="col-span-8">
+                    <h3 className="text-2xl font-bold mb-5">Penny is Mid</h3>
+                    <p className="leading-7 mb-5">There are situations where Penny has value to save a Genesect and remove accumulating damage. However, if you play Penny, you are not disrupting with Judge and are allowing your opponent sit on large hand sizes.</p>
+                    <p className="leading-7 mb-5">It’s stronger to try and disrupt whatever combo your opponent is attempting to build in their hand. Also it’s one matchup.</p>
+                </div>
+            </div>
+            <div className="divider my-5"></div>
+            <div className="grid grid-cols-12 gap-8">
+                <div className="col-span-4">
+                    <img className="w-full" src={overteching} alt="tech cards" />
+                </div>
+                <div className="col-span-8">
+                    <h3 className="text-2xl font-bold mb-5">Overteching</h3>
+                    <p className="leading-7 mb-5">Specific to Le Bui, his list is over teched for no reason. I don’t really see a benefit for playing Avery over Judge in the format. Even in our match I was relieved to see Avery as the supporter for turn instead of the Judge + Path combo. Fan of Waves seems like a complete wasted that could have been better as a third Forest Seal Stone. Two copies is risky as you want to play it every game—especially since your opponents will attempt to Lost Zone them.</p>                    
+                </div>
+            </div>
+            <div className="divider my-5"></div>
+            <p className="leading-7 mb-5">I fully believe that with better draws, or in the command of a better pilot Tord’s list could have made top 8 or even taken down the entire event. Day one felt so smooth and perfect, but my hands in day two were packed with bad luck, rough opening hand decisions, and every round I was forced to discard resources that I knew I couldn't to win.</p>
 
 
         </div>
